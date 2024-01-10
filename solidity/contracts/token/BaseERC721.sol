ba41d0752fda3e6d1008d73f519631981879752f
0xdwong <0xdwong@gmail.com>
2024-01-10 16:34:54 +0800

    add: ERC721 contract and test cases

---
+++ solidity/contracts/token/BaseERC721.sol
@@ -0,0 +1,335 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract BaseERC721 {
+    using Strings for uint256;
+    using Address for address;
+
+    // Token name
+    string private _name;
+
+    // Token symbol
+    string private _symbol;
+
+    // Token baseURI
+    string private _baseURI;
+
+    // Mapping from token ID to owner address
+    mapping(uint256 => address) private _owners;
+
+    // Mapping owner address to token count
+    mapping(address => uint256) private _balances;
+
+    // Mapping from token ID to approved address
+    mapping(uint256 => address) private _tokenApprovals;
+
+    // Mapping from owner to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    /**
+     * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
+     */
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId
+    );
+
+    /**
+     * @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
+     */
+    event Approval(
+        address indexed owner,
+        address indexed approved,
+        uint256 indexed tokenId
+    );
+
+    /**
+     * @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
+     */
+    event ApprovalForAll(
+        address indexed owner,
+        address indexed operator,
+        bool approved
+    );
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory baseURI_
+    ) {
+        // Initializes the contract by setting a `name`, a `symbol` and a `baseURI`
+
+        // Write your code here
+        _name = name_;
+        _symbol = symbol_;
+        _baseURI = baseURI_;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-name}.
+     */
+    function name() public view returns (string memory) {
+        // Write your code here
+        return _name;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-symbol}.
+     */
+    function symbol() public view returns (string memory) {
+        // Write your code here
+        return _symbol;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        // Write your code here
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory baseURI = _baseURI;
+        return string(abi.encodePacked(baseURI, tokenId.toString()));
+    }
+
+    function balanceOf(address owner) public view returns (uint256) {
+        return _balances[owner];
+    }
+
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        address owner = _owners[tokenId];
+        return owner;
+    }
+
+    function approve(address to, uint256 tokenId) public {
+        address owner = ownerOf(tokenId);
+        require(to != owner, "ERC721: approval to current owner");
+
+        address sender = msg.sender;
+        require(
+            sender == owner || isApprovedForAll(owner, sender),
+            "ERC721: approve caller is not owner nor approved for all"
+        );
+
+        _tokenApprovals[tokenId] = to;
+
+        emit Approval(owner, to, tokenId);
+    }
+
+    function getApproved(uint256 tokenId) public view returns (address) {
+        require(
+            _exists(tokenId),
+            "ERC721: approved query for nonexistent token"
+        );
+
+        return _tokenApprovals[tokenId];
+    }
+
+    function setApprovalForAll(address operator, bool approved) public {
+        address sender = msg.sender;
+        require(sender != operator, "ERC721: approve to caller");
+        _operatorApprovals[sender][operator] = approved;
+        emit ApprovalForAll(sender, operator, approved);
+    }
+
+    function isApprovedForAll(
+        address owner,
+        address operator
+    ) public view returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public {
+        require(
+            _isApprovedOrOwner(msg.sender, tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+
+        _transfer(from, to, tokenId);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    /**
+     * @dev See {IERC721-safeTransferFrom}.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public {
+        require(
+            _isApprovedOrOwner(msg.sender, tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        _safeTransfer(from, to, tokenId, _data);
+    }
+
+    function _safeTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) internal {
+        _transfer(from, to, tokenId);
+        require(
+            _checkOnERC721Received(from, to, tokenId, _data),
+            "ERC721: transfer to non ERC721Receiver implementer"
+        );
+    }
+
+    /**
+     * @dev Returns whether `tokenId` exists.
+     *
+     * Tokens can be managed by their owner or approved accounts via {approve} or {setApprovalForAll}.
+     *
+     * Tokens start existing when they are minted (`_mint`),
+     * and stop existing when they are burned (`_burn`).
+     */
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        return _owners[tokenId] != address(0);
+    }
+
+    /**
+     * @dev Returns whether `spender` is allowed to manage `tokenId`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function _isApprovedOrOwner(
+        address spender,
+        uint256 tokenId
+    ) internal view returns (bool) {
+        require(
+            _exists(tokenId),
+            "ERC721: operator query for nonexistent token"
+        );
+        address owner = ownerOf(tokenId);
+        return (spender == owner ||
+            getApproved(tokenId) == spender ||
+            isApprovedForAll(owner, spender));
+    }
+
+    /**
+     * @dev Mints `tokenId` and transfers it to `to`.
+     *
+     *
+     * Requirements:
+     *
+     * - `tokenId` must not exist.
+     * - `to` cannot be the zero address.
+     *
+     * Emits a {Transfer} event.
+     */
+    function mint(address to, uint256 tokenId) public {
+        require(to != address(0), "ERC721: mint to the zero address");
+        require(!_exists(tokenId), "ERC721: token already minted");
+
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
+    }
+
+    /**
+     * @dev Transfers `tokenId` from `from` to `to`.
+     *  As opposed to {transferFrom}, this imposes no restrictions on msg.sender.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must be owned by `from`.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _transfer(address from, address to, uint256 tokenId) internal {
+        require(
+            ownerOf(tokenId) == from,
+            "ERC721: transfer from incorrect owner"
+        );
+        require(to != address(0), "ERC721: transfer to the zero address");
+
+        // Clear approvals from the previous owner
+        _approve(address(0), tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev Approve `to` to operate on `tokenId`
+     *
+     * Emits a {Approval} event.
+     */
+    function _approve(address to, uint256 tokenId) internal virtual {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(ownerOf(tokenId), to, tokenId);
+    }
+
+    function _checkOnERC721Received(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) private returns (bool) {
+        if (to.isContract()) {
+            try
+                IERC721Receiver(to).onERC721Received(
+                    msg.sender,
+                    from,
+                    tokenId,
+                    _data
+                )
+            returns (bytes4 retval) {
+                return retval == IERC721Receiver.onERC721Received.selector;
+            } catch (bytes memory reason) {
+                if (reason.length == 0) {
+                    revert(
+                        "ERC721: transfer to non ERC721Receiver implementer"
+                    );
+                } else {
+                    assembly {
+                        revert(add(32, reason), mload(reason))
+                    }
+                }
+            }
+        } else {
+            return true;
+        }
+    }
+}
+
+contract BaseERC721Receiver is BaseERC721, IERC721Receiver {
+    constructor() BaseERC721("BaseERC721Receiver", "BERC721R", "") {
+        
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
---
+++ solidity/test/token/baseERC721.js
@@ -0,0 +1,400 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+
+describe("BaseERC721", async () => {
+    let contract, contractAddr;
+    let receivercontract, receivercontractAddr;
+    let accounts, owner;
+    const name = 'BaseERC721';
+    const symbol = 'BERC721';
+    const baseURI = 'https://images.example.com/';
+
+    const randomAccount = ethers.Wallet.createRandom();
+    const randomAddr = randomAccount.address;
+    const ZeroAddress = ethers.ZeroAddress;
+
+
+    async function init() {
+        // 部署合约
+        accounts = await ethers.getSigners();
+        owner = accounts[0];
+
+        // const factory = await ethers.getContractFactory('BaseERC721');
+        // contract = await factory.deploy();
+        // await contract.deployed();
+
+        contract = await ethers.deployContract("BaseERC721", [name, symbol, baseURI]);
+        receivercontract = await ethers.deployContract("BaseERC721Receiver");
+
+        contractAddr = await contract.getAddress();
+        receivercontractAddr = await receivercontract.getAddress();
+    }
+
+    beforeEach(async () => {
+        await init();
+    })
+
+
+    describe("IERC721Metadata", async () => {
+        it("name", async () => {
+            expect(await contract.name()).to.equal(name);
+        });
+
+        it("symbol", async () => {
+            expect(await contract.symbol()).to.equal(symbol);
+        });
+
+        describe("tokenURI", async () => {
+            it("URI query for nonexistent token should revert", async () => {
+                const NONE_EXISTENT_TOKEN_ID = 1234
+                await expect(
+                    contract.tokenURI(NONE_EXISTENT_TOKEN_ID)
+                ).to.be.revertedWith("ERC721Metadata: URI query for nonexistent token");
+            });
+
+            it('Should return baseURI when tokenId exists', async function () {
+                const tokenId = 1
+                await contract.connect(owner).mint(randomAddr, tokenId);
+
+                const expectURI = baseURI + String(tokenId);
+                expect(await contract.tokenURI(tokenId)).to.equal(expectURI);
+            });
+        })
+    })
+
+    describe("IERC721", async () => {
+        describe("balanceOf ", async () => {
+            it("balanceOf", async () => {
+                // before should be 0
+                const beforeBalance = await contract.balanceOf(randomAddr);
+                expect(beforeBalance).to.equal(0);
+
+                // mint
+                await contract.connect(owner).mint(randomAddr, 1);
+
+                // after should be 1
+                const afterBalance = await contract.balanceOf(randomAddr);
+                expect(afterBalance).to.equal(1);
+            });
+        });
+
+        describe("ownerOf ", async () => {
+            it("ownerOf", async () => {
+                const tokenId = 1;
+                const receiver = randomAddr;
+                // before should be 0 address
+                const beforeBalance = await contract.ownerOf(tokenId);
+                expect(beforeBalance).to.equal(ZeroAddress);
+
+                // mint
+                await contract.connect(owner).mint(receiver, 1);
+
+                // after should be receiver
+                const holder = await contract.ownerOf(tokenId);
+                expect(holder).to.equal(receiver);
+            });
+        });
+
+        describe('approve', function () {
+            it('owner should approve successfully', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); //mint to self
+
+                const to = randomAddr;
+                await expect(
+                    contract.connect(owner).approve(to, tokenId)
+                ).to.emit(contract, "Approval")
+                    .withArgs(owner.address, to, tokenId);
+
+                // getApproved
+                expect(await contract.getApproved(tokenId)).to.equal(to);
+            });
+
+            it('approved account should approve successfully', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); //mint to self
+
+                // setApprovalForAll accounts1
+                const caller = accounts[1];
+                await contract.connect(owner).setApprovalForAll(caller.address, true)
+
+                // accounts1 arpprove owner's tokenId[1] to randomAddr
+                const to = randomAddr;
+                expect(
+                    await contract.connect(caller).approve(to, tokenId)
+                ).to.be.ok;
+
+                // getApproved
+                expect(await contract.getApproved(tokenId)).to.equal(to);
+            });
+
+            it('Approve to current owner should revert', async function () {
+                // mint token first
+                const tokenId = 1;
+                const receiver = owner.address; //self
+                await contract.connect(owner).mint(receiver, tokenId);
+
+                await expect(
+                    contract.connect(owner).approve(receiver, tokenId)
+                ).to.be.revertedWith("ERC721: approval to current owner");
+            });
+
+            it('Not owner nor approved token approveal should revert', async function () {
+                // mint token first
+                const tokenId = 1;
+                const receiver = owner.address; //self
+                await contract.connect(owner).mint(receiver, tokenId);
+
+                const otherAccount = accounts[1]; //not owner or approved
+                await expect(
+                    contract.connect(otherAccount).approve(randomAddr, tokenId)
+                ).to.be.revertedWith("ERC721: approve caller is not owner nor approved for all");
+            });
+        });
+
+        describe('getApproved', function () {
+            it('should return approval address', async function () {
+                // mint token first
+                const tokenId = 1;
+                const receiver = owner.address; //self
+                await contract.connect(owner).mint(receiver, tokenId);
+
+                // approve
+                const approvedAddr = randomAddr;
+                await contract.connect(owner).approve(randomAddr, tokenId);
+
+                expect(await contract.getApproved(tokenId)).to.equal(approvedAddr);
+            });
+
+            it('Approved query for nonexistent token should revert', async function () {
+                const tokenId = 1; // not exists
+
+                await expect(
+                    contract.getApproved(tokenId)
+                ).to.be.revertedWith('ERC721: approved query for nonexistent token');
+            });
+        });
+
+        describe('setApprovalForAll', function () {
+            it('setApprovalForAll true/flase', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const spender = randomAddr;
+
+                // set true
+                await contract.connect(owner).setApprovalForAll(spender, true);
+                expect(await contract.isApprovedForAll(owner.address, spender)).to.equal(true);
+
+                // set false
+                await contract.connect(owner).setApprovalForAll(spender, false);
+                expect(await contract.isApprovedForAll(owner.address, spender)).to.equal(false);
+            });
+
+            it('Approve to self should revert', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                await expect(
+                    contract.connect(owner).setApprovalForAll(owner.address, true) // approve to self
+                ).to.be.revertedWith("ERC721: approve to caller");
+            });
+        });
+
+        describe('transferFrom', function () {
+            it('owner account should succeed and balance should change', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = randomAddr;
+
+                // balance change
+                await expect(
+                    contract.connect(owner).transferFrom(owner.address, to, tokenId)
+                ).to.changeTokenBalances(contract, [owner.address, to], [-1, 1]);
+            });
+
+            it('approved account should succeed and balance should change', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = randomAddr;
+
+                // approve
+                const spenderAccout = accounts[1];
+                await contract.connect(owner).approve(spenderAccout.address, tokenId)
+
+                // transfer and balance should change
+                await expect(
+                    contract.connect(spenderAccout).transferFrom(owner.address, to, tokenId)
+                ).to.changeTokenBalances(contract, [owner.address, to], [-1, 1]);
+            });
+
+            it('approvedForAll account should succeed and balance should change', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = randomAddr;
+
+                // setApprovalForAll
+                const spenderAccout = accounts[1];
+                await contract.connect(owner).setApprovalForAll(spenderAccout.address, true)
+
+                // transfer and balance should change
+                await expect(
+                    contract.connect(spenderAccout).transferFrom(owner.address, to, tokenId)
+                ).to.changeTokenBalances(contract, [owner.address, to], [-1, 1]);
+            });
+
+            it('not owner nor approved should revert', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = randomAddr;
+                const otherAccount = accounts[1]; //not owner or approved
+                await expect(
+                    contract.connect(otherAccount).transferFrom(owner.address, to, tokenId)
+                ).to.revertedWith("ERC721: transfer caller is not owner nor approved");
+            });
+
+            it('none exists tokenId should revert', async function () {
+                const NONE_EXISTENT_TOKEN_ID = Math.ceil(Math.random() * 1000000);
+                const to = randomAddr;
+                await expect(
+                    contract.connect(owner).transferFrom(owner.address, to, NONE_EXISTENT_TOKEN_ID)
+                ).to.revertedWith("ERC721: operator query for nonexistent token");
+            });
+
+            it('to zero address should revert', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = ZeroAddress;
+                await expect(
+                    contract.connect(owner).transferFrom(owner.address, to, tokenId)
+                ).to.revertedWith("ERC721: transfer to the zero address");
+            });
+
+            it('from != caller.address should revert', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = randomAddr;
+                const from = accounts[1].address;
+                await expect(
+                    contract.connect(owner).transferFrom(from, to, tokenId)
+                ).to.revertedWith("ERC721: transfer from incorrect owner");
+            });
+        });
+
+        describe('safeTransferFrom', function () {
+            // same as transferFrom
+            it('owner should succeed and balance should change', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = randomAddr;
+
+                // balance change
+                await expect(
+                    contract.connect(owner)["safeTransferFrom(address,address,uint256)"](owner.address, to, tokenId)
+                ).to.changeTokenBalances(contract, [owner.address, to], [-1, 1]);
+            });
+
+            // same as transferFrom
+            it('approved should succeed and balance should change', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = randomAddr;
+
+                // approve
+                const spenderAccout = accounts[1];
+                await contract.connect(owner).approve(spenderAccout.address, tokenId)
+
+                // transfer and balance should change
+                await expect(
+                    contract.connect(spenderAccout)["safeTransferFrom(address,address,uint256)"](owner.address, to, tokenId)
+                ).to.changeTokenBalances(contract, [owner.address, to], [-1, 1]);
+            });
+
+            // same as transferFrom
+            it('not owner nor approved should revert', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = randomAddr;
+                const otherAccount = accounts[1]; //not owner or approved
+                await expect(
+                    contract.connect(otherAccount)["safeTransferFrom(address,address,uint256)"](owner.address, to, tokenId)
+                ).to.revertedWith("ERC721: transfer caller is not owner nor approved");
+            });
+
+            it('transfer to none ERC721Receiver implementer should revert', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = contractAddr; // not support ERC721Receiver
+                await expect(
+                    contract.connect(owner)["safeTransferFrom(address,address,uint256)"](owner.address, to, tokenId)
+                ).to.revertedWith("ERC721: transfer to non ERC721Receiver implementer");
+            });
+
+            it('transfer to ERC721Receiver implementer should succeed', async function () {
+                // mint token first
+                const tokenId = 1;
+                await contract.connect(owner).mint(owner.address, tokenId); // mint to self
+
+                const to = receivercontractAddr; // support ERC721Receiver
+                expect(
+                    await contract.connect(owner)["safeTransferFrom(address,address,uint256)"](owner.address, to, tokenId)
+                ).to.be.ok;
+            });
+        });
+    })
+
+    describe("mint", async () => {
+        it('mint succeed should update balance', async function () {
+            const tokenId = 1;
+
+            await expect(
+                contract.connect(owner).mint(randomAddr, tokenId)
+            ).to.changeTokenBalance(contract, randomAddr, 1);
+        });
+
+        it("mint to the zero address should revert", async () => {
+            const tokenId = 1;
+
+            await expect(
+                contract.connect(owner).mint(ZeroAddress, tokenId)
+            ).to.be.revertedWith("ERC721: mint to the zero address");
+        });
+
+        it("mint repeated tokenId should revert", async () => {
+            const tokenId = 1;
+
+            // first mint
+            await contract.connect(owner).mint(randomAddr, tokenId)
+
+            // sencond
+            await expect(
+                contract.connect(owner).mint(randomAddr, tokenId)
+            ).to.be.revertedWith("ERC721: token already minted");
+        });
+    })
+});
